1aa7f332a0ed2b3661a7c282959424b95b56dbcd
NieznanyJ <101598645+NieznanyJ@users.noreply.github.com>
2023-10-17 19:20:55 +0200

    v

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/audio/",
+  base:"https://nieznanyj.github.io/audio/",
   plugins: [react()],
 })
